3482fc0eec3adbce62ac9b60d0db44e47db107e8
AshikMuezza <ashik@bitscunch.com>
2022-10-17 02:25:11 +0200

    Custom Navbar added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import {Feature,Navbar} from './components'
+import {Header,Footer} from './containers'
+import './App.css'
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className='gradient__bg'>
+        <Navbar/>
+      </div>
+    </div>
+  )
+}
+
+export default App;
